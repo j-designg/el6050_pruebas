d0043928fffe701cb7f2ecc9f94de9d74b11e16d
Jose Gonzalez <jgonzalezg@outlook.cl>
2021-03-25 15:33:26 -0300

    Plot de sin(x) - y

--- prueba.py
+++ prueba.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+y = np.linspace(0, 20, 100)
 
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.plot(x, np.sin(x) - y)       # Plot the sine of each x point
 plt.show()                   # Display the plot
