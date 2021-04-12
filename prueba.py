0ff18265dbd71bff8691f63bd0c5ec9faf792dfb
José Manuel Gonzalez Garcia <jgonzalezg@outlook.cl>
2021-04-12 19:32:15 -0400

    Actualización de pruebas.py

--- prueba.py
+++ prueba.py
@@ -3,6 +3,7 @@ import numpy as np
 
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 y = np.linspace(0, 20, 100)
+z = np.linspace(0, 20, 100)
 
 plt.plot(x, np.sin(x) + 2*y)       # Plot the sine of each x point
 plt.show()                   # Display the plot
